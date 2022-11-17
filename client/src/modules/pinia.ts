b0c3f9614679163b39a4aea9f86f52f60b26454c
hamza mellahi <hmellahi@leyton.com>
2022-11-17 23:27:11 +0100

    cleaning

--- client/src/modules/pinia.ts
+++ client/src/modules/pinia.ts
@@ -6,9 +6,7 @@ import { createPinia } from 'pinia';
 export const install: ViteSetupModule = ({ isClient, initialState, app }) => {
 	const pinia = createPinia();
 	app.use(pinia);
-	// Refer to
-	// https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
-	// for other serialization strategies.
+
 	if (isClient) pinia.state.value = initialState.pinia || {};
 	else initialState.pinia = pinia.state.value;
 };
