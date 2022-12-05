99b3876f8f8903a7e3a108484225197eff216a6e
hamza mellahi <hmellahi@leyton.com>
2022-12-05 22:47:17 +0100

    cleaning

--- server/src/components/Auth/auth.module.js
+++ server/src/components/Auth/auth.module.js
@@ -14,6 +14,7 @@ import AuthService from "./auth.service";
 import AuthController from "./auth.controller";
 import setupAuthRoutes from "./auth.routes";
 
+
 const {makeValidatorCallback, responseCallback} = middlewares;
 
 // routes
