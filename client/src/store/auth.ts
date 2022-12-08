8ef2e5580ac7fa5b6675fee3ae1aa3192671d406
hamza mellahi <hmellahi@leyton.com>
2022-12-08 19:09:24 +0100

    redirect to /emailsent page

--- client/src/store/auth.ts
+++ client/src/store/auth.ts
@@ -1,17 +1,21 @@
 import { defineStore } from 'pinia';
 import { login, registerUser } from '../services/authService';
 
-export const useAuthStore = defineStore('auth', {
-	state: () => ({
-		user: null,
-	}),
-	actions: {
-		logIn(username: string, password: string) {
-			console.log(username, password);
-			login(username, password);
+export const useAuthStore = () => {
+	const router = useRouter();
+	return defineStore('auth', ({
+		state: () => ({
+			user: null,
+		}),
+		actions: {
+			logIn(username: string, password: string) {
+				console.log(username, password);
+				login(username, password);
+			},
+			async register(newUser){
+				await registerUser(newUser).catch();
+				router.push({ path:'/EmailSent' }); 
+			}
 		},
-		register(newUser){
-			registerUser(newUser);
-		}
-	},
-});
+	}))()
+};
