a8da810151c2cfb3e945fa59b79b4177a39c35a7
hamzamellahi <hamzaSenpai307@gmail.com>
2022-12-10 23:41:39 +0100

    Register font awesome

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,14 @@ import { ViteSSG } from 'vite-ssg';
 import '@/styles/index.css';
 import { extendedRoutes } from '@/router';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(faUserSecret);
 
 export const createApp = ViteSSG(
 	App,
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,23 +1,34 @@
 <script setup lang="ts">
+	/* import font awesome icon component */
+	import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 	const user = {
 		first_name: 'RebiKaa',
 		last_name: 'Talon',
 		age: 24,
-		distance: '14km',
+		distance: '14 km',
 		bio: 'looking for friends.',
 	};
 </script>
 
 <template>
 	<div class="bg-[#F6F7FF] h-screen">
-		<div class="w-4/12 bg-white shadow-black shadow-sm my-6 mx-auto">
-			<img src="/girl.jpeg" class="min-w-full h-[37rem]" />
-			<div class="px-10 py-10">
-				<div class="text-2xl flex items-center gap-x-3">
+		<div
+			class="w-4/12 bg-white shadow-slate-300 shadow-sm mt-6 mx-auto rounded-[2rem]"
+		>
+			<img
+				src="/girl.jpeg"
+				class="min-w-full h-[37rem] rounded-t-[2rem]"
+			/>
+			<div class="px-8 py-8">
+				<div
+					class="font-medium text-[#48496B] text-2xl flex items-center gap-x-3"
+				>
 					{{ user.first_name }} {{ user.last_name }}, {{ user.age }}
-					<span class="dot bg-green-500 w-3 h-3"></span>
+					<span class="dot bg-[#4EB3AC] w-3 h-3"></span>
 				</div>
+				<FontAwesomeIcon icon="fa-solid fa-location-pin" />
 				<div class="text-md text-[#B7B5BF]">
+					<FontAwesomeIcon icon="fa-solid fa-location-arrow" />
 					{{ user.distance }} from you.
 				</div>
 				<div class="text-lg">{{ user.bio }}</div>
