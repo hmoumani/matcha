e266023a620b52fd99917c75c5b94d009fe8b303
hamzamellahi <hamzaSenpai307@gmail.com>
2022-12-10 23:14:53 +0100

    design profile card

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -10,20 +10,29 @@
 
 <template>
 	<div class="bg-[#F6F7FF] h-screen">
-		<div class="w-5/12 bg-white  px-10 py-20 shadow-black shadow-sm">
-			<div class="text-2xl"> 
-				{{ user.first_name }} {{ user.last_name }}, {{ user.age }}
+		<div class="w-4/12 bg-white shadow-black shadow-sm my-6 mx-auto">
+			<img src="/girl.jpeg" class="min-w-full h-[37rem]" />
+			<div class="px-10 py-10">
+				<div class="text-2xl flex items-center gap-x-3">
+					{{ user.first_name }} {{ user.last_name }}, {{ user.age }}
+					<span class="dot bg-green-500 w-3 h-3"></span>
+				</div>
+				<div class="text-md text-[#B7B5BF]">
+					{{ user.distance }} from you.
+				</div>
+				<div class="text-lg">{{ user.bio }}</div>
+				<div>Passions:</div>
 			</div>
-			<div class="text-md text-[#B7B5BF]">
-				{{ user.distance }} from you.
-			</div>  
-			<div class="text-lg">{{ user.bio }} from you.</div>
-			<div>Passions: </div> 
 		</div>
 	</div>
 </template>
 
-<style></style>
+<style>
+	.dot {
+		border-radius: 50%;
+		display: inline-block;
+	}
+</style>
 <route lang="yaml">
 name: home
 </route>
