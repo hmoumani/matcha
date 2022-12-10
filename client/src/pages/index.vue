4132250d7bcea5f38c3272436557dda678887091
hamza mellahi <hamzasenpai307@gmail.com>
2022-12-10 22:34:25 +0100

    add feed page

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,50 +1,29 @@
 <script setup lang="ts">
-	import { useStore } from '@/store';
-	import VitailseLogo from '@/assets/logo.png';
-	useHead({
-		title: 'Vitailse | Opinionated vite starter template',
-	});
-
-	const store = useStore();
+	const user = {
+		first_name: 'RebiKaa',
+		last_name: 'Talon',
+		age: 24,
+		distance: '14km',
+		bio: 'looking for friends.',
+	};
 </script>
 
 <template>
-	<div class="grid place-items-center min-h-[80vh]">
-		<div>
-			<img :src="VitailseLogo" width="300" class="mx-auto" />
-			<p class="lg:text-left text-center">
-				<a
-					href="https://tailwindcss.com/"
-					class="text-blue-500 hover:underline"
-					>TailwindCSS</a
-				>
-			</p>
-			<p class="text-center">
-				<a
-					class="text-blue-500 hover:underline"
-					href="https://github.com/zynth17/vitailse"
-					>Github Repo</a
-				>
-			</p>
-			<div class="mt-5 text-center">
-				<button
-					@click="store.$state.count++"
-					class="
-						px-4
-						py-2
-						dark:bg-blue-800
-						bg-blue-500
-						text-white
-						rounded
-					"
-				>
-					Count : {{ store.$state.count }}
-				</button>
+	<div class="bg-[#F6F7FF] h-screen">
+		<div class="w-5/12 bg-white  px-10 py-20 shadow-black shadow-sm">
+			<div class="text-2xl">
+				{{ user.first_name }} {{ user.last_name }}, {{ user.age }}
 			</div>
-		</div>	
+			<div class="text-md text-[#B7B5BF]">
+				{{ user.distance }} from you.
+			</div>
+			<div class="text-lg">{{ user.bio }} from you.</div>
+			<div>Passions</div>
+		</div>
 	</div>
 </template>
 
+<style></style>
 <route lang="yaml">
 name: home
 </route>
