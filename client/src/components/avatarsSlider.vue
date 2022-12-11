5fe4303d3e70981a2d206822e62861ee80cb0290
hamzamellahi <hamzaSenpai307@gmail.com>
2022-12-11 02:16:19 +0100

    add custom nav

--- client/src/components/avatarsSlider.vue
+++ client/src/components/avatarsSlider.vue
@@ -1,28 +1,44 @@
 <template>
-	<carousel :items-to-show="1.5">
-		<slide v-for="slide in 10" :key="slide">
-			{{ slide }}
+	<carousel :items-to-show="1" v-model="currentSlide" class="relative">
+		<slide v-for="avatarUrl in avatars" :key="avatar">
+			<img
+				:src="avatarUrl"
+				class="min-w-full h-[32rem] rounded-t-[2rem]"
+			/>
 		</slide>
 
 		<template #addons>
-			<navigation />
+			<!-- <navigation /> -->
+			<div
+				class="absolute top-[50%] text-white flex justify-between w-full"
+			>
+				<FontAwesomeIcon
+					@click="next"
+					icon="fa-solid fa-arrow-right"
+					class="text-black bg-[#F8F7FF] action px-10"
+				/>
+                <FontAwesomeIcon
+					@click="next"
+					icon="fa-solid fa-arrow-left"
+					class="text-black bg-[#F8F7FF] action px-10"
+				/>
+			</div>
 			<pagination />
 		</template>
 	</carousel>
 </template>
 
-<script>
+<script setup>
 	// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
 	import 'vue3-carousel/dist/carousel.css';
 	import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
-	export default {
-		name: 'App',
-		components: {
-			Carousel,
-			Slide,
-			Pagination,
-			Navigation,
-		},
-	};
+	defineProps({
+		avatars: Array,
+	});
+
+	const currentSlide = ref(0);
+
+	const next = () => currentSlide.value++;
+	const prev = () => currentSlide.value--;
 </script>
