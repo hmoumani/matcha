c1e2a8d2e01acd90a49da5df1ea0187abf8d5562
Houssam-Eddine MOUMANIS <hmoumanis@leyton.com>
2022-12-22 20:44:43 +0100

    setup db and db connection with backend

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,10 @@ const app = express();
 
 import cors from 'cors';
 
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 // error handler
 import 'express-async-errors';
 
---
+++ server/src/db/index.js
@@ -0,0 +1,27 @@
+import pg from 'pg'
+
+const pool = new pg.Pool();
+
+async function query(text, params) {
+    // invocation timestamp for the query method
+    const start = Date.now();
+    try {
+        const res = await pool.query(text, params);
+        // time elapsed since invocation to execution
+        const duration = Date.now() - start;
+        console.log(
+            'executed query', 
+            {text, duration, rows: res.rowCount}
+        );
+        return res;
+    } catch (error) {
+        console.log('error in query', {text});
+        throw error;
+    }
+};
+
+function hello_world() {
+    console.log("hello world");
+} 
+
+export { query, hello_world };
