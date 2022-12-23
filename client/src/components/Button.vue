c3b485193f7adeb35227d4166558eede90cfe329
hamzamellahi <hamzaSenpai307@gmail.com>
2022-12-23 18:36:15 +0100

    test contributions

--- client/src/components/Button.vue
+++ client/src/components/Button.vue
@@ -3,6 +3,6 @@
 		class="text-orange-main text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
 		type="button"
 	>
-		<slot></slot>
+		<slot></slot> 
 	</button>
 </template>
