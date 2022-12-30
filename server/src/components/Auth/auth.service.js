d9e36cc576f8680b52b17d31c1ec33b62520aedd
Houssam-Eddine MOUMANIS <hmoumanis@leyton.com>
2022-12-30 17:31:55 +0100

    REFACTOR - remove nested if conditions.

--- server/src/components/Auth/auth.service.js
+++ server/src/components/Auth/auth.service.js
@@ -14,15 +14,12 @@ const AuthService = {
   login: async (requestBody) => {
     try {
       let results = await query("select * from users where username = $1 or email = $1", [requestBody.username]);
-      if (results.rowCount > 0) {
-        let passwordIsValid = bcrypt.compareSync(requestBody.password, results.rows[0].password);
-        if (!passwordIsValid) {
-          return {status_code: 401, message: "Invalid Password!"};
-        }        
-        return {status_code: 200, message: "User logged in successfully!", user: results.rows[0]};
-      } else {
+      if (results.rowCount <= 0)
         return {status_code: 404, message: "User not found!"};
-      }
+      let passwordIsValid = bcrypt.compareSync(requestBody.password, results.rows[0].password);
+      if (!passwordIsValid)
+        return {status_code: 401, message: "Invalid Password!"};
+      return {status_code: 200, message: "User logged in successfully!", user: results.rows[0]};
     } catch (error) {
       return {status_code: 409, message: "Unable to login user!"};
     }
