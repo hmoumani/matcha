1dfca2988c3a772f7ba646e631f5d0b9354b0f01
Houssam-Eddine MOUMANIS <hmoumanis@leyton.com>
2023-01-08 17:53:37 +0100

    feat - add password reset function in services .

--- server/src/components/Auth/auth.service.js
+++ server/src/components/Auth/auth.service.js
@@ -32,12 +32,27 @@ const AuthService = {
     return {status_code: 200, message: "User registered successfully!", registredUserId: results.rows[0].id};
   },
   verifyEmail: async (requestBody) => {
-    let results = await query("select * from validation_tokens where token=$1", [requestBody.token]);
+    let results = await query("select * from validation_tokens where token=$1 and token_type=$2", [requestBody.token, "email"]);
     if (results.rowCount <= 0)
       throw new Error("Invalid token");
     await query("update users set is_email_verified = true where id = $1", [results.rows[0].user_id]);
     await query("delete from validation_tokens where token = $1", [requestBody.token]);
     return {status_code: 200, message: "Email verified successfully!"};
+  },
+  resetPassword: async (requestBody) => {
+    let results = await query("select * from validation_tokens where token=$1 and token_type=$2", [requestBody.token, "password"]);
+    const password = bcrypt.hashSync(requestBody.password, 8);
+    if (results.rowCount <= 0)
+      throw new Error("Invalid token");
+    await query("update users set password = $1 where id = $2", [password, results.rows[0].user_id]);
+    await query("delete from validation_tokens where token = $1", [requestBody.token]);
+    return {status_code: 200, message: "password updated successfully!"};
+  },
+  getUserIdByEmail: async (email) => {
+    let results = await query("select id from users where email=$1", [email]);
+    if (results.rowCount <= 0)
+      throw new Error("Invalid email");
+    return results.rows[0].id;
   }
 };
 
