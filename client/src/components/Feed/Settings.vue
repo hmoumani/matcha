43db2e754cedf086f36fd9ac3ed1879fb1df011f
hamzamellahi <hamzaSenpai307@gmail.com>
2023-01-18 13:13:36 +0100

    refactor passion tags

--- client/src/components/Feed/Settings.vue
+++ client/src/components/Feed/Settings.vue
@@ -4,11 +4,25 @@
 	>
 		<div>
 			<h2 class="mb-3">Common Tags</h2>
-			<!-- <passionTags /> -->
+			<passionTags :passions="currentUser.passions"/>
 		</div>
 		<div>
-			<h2 class="mb-3">Fame Rating</h2>
-			<Map class="w-full h-[18rem]"/>
+			<h2 class="mb-3">Location</h2>
+			<Map class="" />
+		</div>
+		<div>
+			<RangeSlider />
 		</div>
 	</div>
 </template>
+<script setup>
+import { useUserStore } from '@/store/user';
+import { storeToRefs } from 'pinia';
+
+const userStore = useUserStore();
+let { currentUser } = storeToRefs(userStore);
+
+const handleChangePassions = (passions) {
+	currentUser.value.passions = passions;
+}
+</script>
