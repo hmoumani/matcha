b0012975df0dde55ad7163a13a57dcb85bb5ea66
hamzamellahi <hamzaSenpai307@gmail.com>
2023-01-21 00:37:59 +0100

    range slider componetn

--- client/src/components/Feed/rangeSlider.vue
+++ client/src/components/Feed/rangeSlider.vue
@@ -1,48 +1,16 @@
 <template>
 	<div class="multi-range-slider-container">
 		<MultiRangeSlider
-			:baseClassName="multi - range - slider"
 			:min="0"
 			:max="100"
 			:step="1"
-			:ruler="true"
-			:label="true"
+			:ruler="false"
+			:label="false"
 			:minValue="barMinValue"
 			:maxValue="barMaxValue"
 			@input="UpdateValues"
 		/>
 	</div>
-
-	<!-- <div class="flex w-64 m-auto items-center h-32 justify-center">
-		<div class="py-1 relative min-w-full">
-			<div class="h-2 bg-gray-200 rounded-full">
-				<div
-					class="absolute h-2 rounded-full bg-teal-600 w-0"
-					style="width: 24.1935%; left: 11.2903%"
-				></div>
-				<div
-					class="absolute h-4 flex items-center justify-center w-4 rounded-full bg-white shadow border border-gray-300 -ml-2 top-0 cursor-pointer"
-					unselectable="on"
-					onselectstart="return false;"
-					style="left: 11.2903%"
-				></div>
-				<div
-					class="absolute h-4 flex items-center justify-center w-4 rounded-full bg-white shadow border border-gray-300 -ml-2 top-0 cursor-pointer"
-					unselectable="on"
-					onselectstart="return false;"
-					style="left: 35.4839%"
-				></div>
-				<div class="absolute text-gray-800 -ml-1 bottom-0 left-0 -mb-6">
-					$ 8
-				</div>
-				<div
-					class="absolute text-gray-800 -mr-1 bottom-0 right-0 -mb-6"
-				>
-					$ 70
-				</div>
-			</div>
-		</div>
-	</div> -->
 </template>
 <script setup>
 	import MultiRangeSlider from 'multi-range-slider-vue';
@@ -58,6 +26,18 @@
 <style>
 	.multi-range-slider-container {
 		max-width: 500px;
-		margin: 50px auto;
+		margin: 0px auto;
+	}
+	.multi-range-slider{
+		@apply !border-0 !shadow-none !p-0;
+	}
+	.thumb::before{
+		@apply !shadow-none
+	}
+	.multi-range-slider .bar-inner{
+		@apply bg-primary-color !shadow-none !border-none
+	}
+	.bar-left, .bar-right{
+		@apply bg-white !shadow-none
 	}
 </style>
