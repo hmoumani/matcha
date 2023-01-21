166f9554adf5f09d1fde61749c74631de0905f1b
hamzamellahi <hamzaSenpai307@gmail.com>
2023-01-21 14:06:13 +0100

    make slider reusable

--- client/src/components/Feed/rangeSlider.vue
+++ client/src/components/Feed/rangeSlider.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="multi-range-slider-container">
 		<MultiRangeSlider
-			:min="0"
-			:max="100"
-			:step="1"
+			:min="min"
+			:max="max"
+			:step="step"
 			:ruler="false"
 			:label="false"
 			:minValue="barMinValue"
@@ -11,16 +11,31 @@
 			@input="UpdateValues"
 		/>
 	</div>
+	<div class="flex justify-between p-2">
+		<div>
+			{{ barMinValue }}
+		</div>
+		<div>{{ barMaxValue }}</div>
+	</div>
 </template>
 <script setup>
 	import MultiRangeSlider from 'multi-range-slider-vue';
 
-	let barMinValue = 10;
-	let barMaxValue = 90;
+	defineProps({
+		barMinValue:String,
+		barMaxValue:String,
+		min:Number,
+		max:Number,
+		step: {
+			type: Number,
+			default: 1
+		}
+	})
 
-	function UpdateValues(e) {
-		barMinValue = e.minValue;
-		barMaxValue = e.maxValue;
+	const emit = defineEmits(["onChange"]);
+
+	function UpdateValues(data) {
+		emit("onChange", data);
 	}
 </script>
 <style>
@@ -28,16 +43,17 @@
 		max-width: 500px;
 		margin: 0px auto;
 	}
-	.multi-range-slider{
-		@apply !border-0 !shadow-none !p-0;
+	.multi-range-slider {
+		@apply !border-0 !shadow-sm !p-0;
 	}
-	.thumb::before{
-		@apply !shadow-none
+	.thumb::before {
+		@apply !shadow-none;
 	}
-	.multi-range-slider .bar-inner{
-		@apply bg-primary-color !shadow-none !border-none
+	.multi-range-slider .bar-inner {
+		@apply bg-primary-color !shadow-none !border-none;
 	}
-	.bar-left, .bar-right{
-		@apply bg-white !shadow-none
+	.bar-left,
+	.bar-right {
+		@apply !bg-slate-100 !shadow-none ;
 	}
 </style>
