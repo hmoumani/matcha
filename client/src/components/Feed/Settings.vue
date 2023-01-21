ec1e45e6896db0f1459fcc235217669c9df89567
hamzamellahi <hamzaSenpai307@gmail.com>
2023-01-21 14:02:36 +0100

    add age and fame rate slider

--- client/src/components/Feed/Settings.vue
+++ client/src/components/Feed/Settings.vue
@@ -1,43 +1,68 @@
 <script setup>
-import { useUserStore } from '@/store/user';
-import { storeToRefs } from 'pinia';
+	import { useUserStore } from '@/store/user';
+	import { storeToRefs } from 'pinia';
 
-const userStore = useUserStore();
-let { currentUser } = storeToRefs(userStore);
+	const userStore = useUserStore();
+	let { currentUser } = storeToRefs(userStore);
 
-const handleChangePassions = (passions) => {
-	currentUser.value.passions = passions;
-}
+	const handleChangePassions = passions => {
+		currentUser.value.passions = passions;
+	};
 
-const updateLocation = (location) => {
-	console.log(location)
-	// TODO: save search loacation 
-}
+	const updateLocation = location => {
+		console.log(location);
+		// TODO: save search loacation
+	};
 
-const showMap = ref(true);
+	const showMap = ref(true);
+
+	const settings = ref({
+		minAge: 20,
+		maxAge: 40,
+		minFameRating:3,
+		maxFameRating:10
+	});
+
+	const UpdateValues = input => {
+		settings.value.minAge = input.minValue;
+		settings.value.maxAge = input.maxValue;
+	};
+	
 </script>
 <template>
 	<div
-		class="w-3/12 h-[calc(100vh-10rem)] bg-white p-6 rounded-lg shadow-sm flex flex-col gap-y-4"
+		class="w-3/12 h-[calc(100vh-6rem)] bg-white p-6 rounded-lg shadow-sm flex flex-col gap-y-4"
 	>
 		<div>
 			<h2 class="mb-3">Common Tags</h2>
-			<passionTags :passions="currentUser.passions" @on-tags-changed="handleChangePassions"/>
+			<passionTags
+				:passions="currentUser.passions"
+				@on-tags-changed="handleChangePassions"
+			/>
 		</div>
 		<div>
 			<h2 class="mb-3">Location</h2>
-			<Map v-if="showMap" class="" @updateLocation="updateLocation"/>
+			<Map v-if="showMap" class="" @updateLocation="updateLocation" />
 		</div>
 		<div>
-			<!-- <RangeSlider /> -->
+			<h2 class="mb-3">Age</h2>
+			<RangeSlider
+				:barMinValue="settings.minAge"
+				:barMaxValue="settings.maxAge"
+				:min="18"
+				:max="100"
+				@onChange="UpdateValues"
+			/>
 		</div>
-		<!-- <div class="bg-blue-400 w-[13rem] h-10 text-white rounded-md flex jusdtify-center items-center ">
-			<div class="w-2/12 border-r-2 p-3">
-				B
-			</div>
-			<div class="pl-3">
-				Travel to this place
-			</div>
-		</div> -->
+		<div>
+			<h2 class="mb-3">Fame Rate</h2>
+			<RangeSlider
+				:barMaxValue="settings.minFameRating"
+				:barMinValue="settings.maxFameRating"
+				:min="1"
+				:max="10"
+				@onChange="UpdateValues"
+			/>
+		</div>
 	</div>
 </template>
