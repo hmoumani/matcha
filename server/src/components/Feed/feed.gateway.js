aaeee3c8739195c6ea574f94fceea1d34b366d0a
hamzamellahi <hamzaSenpai307@gmail.com>
2023-01-31 13:40:31 +0100

    add matching when a user likes someone

--- server/src/components/Feed/feed.gateway.js
+++ server/src/components/Feed/feed.gateway.js
@@ -5,11 +5,30 @@ import feedService from './feed.service';
 const USER_LIKE_EVENT = 'userLike';
 const USER_DIS_LIKE_EVENT = 'userUnMatch';
 
+const matchedMessage = (user) => `you are matched with ${user.first_name} ${user.last_name}`;
+
 const onUserLikeCallback = async (socket, likedUserId) => {
-  const liker = await UserService.find(socket.userId);
-  await feedService.like(liker.id, likedUserId);
-  const msg = `${liker?.first_name} ${liker?.last_name} liked your profile`;
-  emitToUser(likedUserId, USER_LIKE_EVENT, { liker, msg });
+  const currentUserId = socket.userId;
+  if (feedService.isUserLikedBy(currentUserId, likedUserId)) {
+    return;
+  }
+  const currentUser = await UserService.find(currentUserId);
+
+  await feedService.like(currentUserId, currentUserId);
+
+  // if the likerUser isn't already liked by the use he wanna like
+  if (!feedService.isUserLikedBy(likedUserId, currentUserId)) {
+    const msg = `${currentUser?.first_name} ${currentUser?.last_name} liked your profile`;
+    emitToUser(likedUserId, USER_LIKE_EVENT, { currentUser, msg });
+  }
+  // otherwise
+  // acknowledge both of them that they are matched
+  // acknowledge the liked user
+  const msg = matchedMessage(currentUser);
+  emitToUser(likedUserId, USER_LIKE_EVENT, { currentUser, msg });
+  // acknowledge the current user
+  const likedUser = await UserService.find(socket.userId);
+  emitToUser(likerUser, USER_LIKE_EVENT, { likedUser, msg: matchedMessage(likedUser) });
 };
 
 const onUserDisLikeCallback = async (socket, disLikedUserId) => {
@@ -21,5 +40,7 @@ const onUserDisLikeCallback = async (socket, disLikedUserId) => {
 
 export default (userGatewaySocket) => {
   userGatewaySocket.on(USER_LIKE_EVENT, (likedUserId) => onUserLikeCallback(userGatewaySocket, likedUserId));
-  userGatewaySocket.on(USER_DIS_LIKE_EVENT, (disLikedUserId) => onUserDisLikeCallback(userGatewaySocket, disLikedUserId));
+  userGatewaySocket.on(USER_DIS_LIKE_EVENT, (disLikedUserId) =>
+    onUserDisLikeCallback(userGatewaySocket, disLikedUserId)
+  );
 };
