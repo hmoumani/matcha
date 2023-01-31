576eae606551f660023cdf728be39c876d317444
hamzamellahi <hamzaSenpai307@gmail.com>
2023-01-31 13:49:14 +0100

    cleaning

--- server/src/components/Feed/feed.gateway.js
+++ server/src/components/Feed/feed.gateway.js
@@ -4,8 +4,10 @@ import feedService from './feed.service';
 
 const USER_LIKE_EVENT = 'userLike';
 const USER_DIS_LIKE_EVENT = 'userUnMatch';
+const USER_MATCH_EVENT = 'userMatch';
 
 const matchedMessage = (user) => `you are matched with ${user.first_name} ${user.last_name}`;
+const userLikeMessage = (user) => `${user?.first_name} ${user?.last_name} liked your profile`;
 
 const onUserLikeCallback = async (socket, likedUserId) => {
   const currentUserId = socket.userId;
@@ -18,17 +20,18 @@ const onUserLikeCallback = async (socket, likedUserId) => {
 
   // if the likerUser isn't already liked by the use he wanna like
   if (!feedService.isUserLikedBy(likedUserId, currentUserId)) {
-    const msg = `${currentUser?.first_name} ${currentUser?.last_name} liked your profile`;
-    emitToUser(likedUserId, USER_LIKE_EVENT, { currentUser, msg });
+    emitToUser(likedUserId, USER_LIKE_EVENT, { currentUser, msg: userLikeMessage(currentUser) });
+    return;
   }
+
   // otherwise
   // acknowledge both of them that they are matched
   // acknowledge the liked user
   const msg = matchedMessage(currentUser);
-  emitToUser(likedUserId, USER_LIKE_EVENT, { currentUser, msg });
+  emitToUser(likedUserId, USER_MATCH_EVENT, { currentUser, msg });
   // acknowledge the current user
   const likedUser = await UserService.find(socket.userId);
-  emitToUser(likerUser, USER_LIKE_EVENT, { likedUser, msg: matchedMessage(likedUser) });
+  emitToUser(likerUser, USER_MATCH_EVENT, { likedUser, msg: matchedMessage(likedUser) });
 };
 
 const onUserDisLikeCallback = async (socket, disLikedUserId) => {
