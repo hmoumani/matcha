c90742597340a929bd166483e2cd8650e4afd289
Houssam-Eddine Moumanis <houssam.moumanis@gmail.com>
2023-03-17 22:46:05 +0100

    feat - send notification on new message event.

--- server/src/components/Chat/chat.gateway.js
+++ server/src/components/Chat/chat.gateway.js
@@ -2,9 +2,11 @@ import { emitToUser } from '../User/user.gateway';
 import MessageModel from '../../models/MessageModel';
 import feedService from '../Feed/feed.service';
 import UserService from '../User/user.service';
+import NotificationModel from '../../models/NotificationModel';
 
 const SEND_MESSAGE_EVENT = 'sendMessage';
 const RECEIVE_MESSAGE_EVENT = 'receiveMessage';
+const USER_MESSAGE_NOTIFICATION_EVENT = 'userMessageNotification';
 
 const onSendMessageCallback = async (socket, message) => {
   try{
@@ -25,6 +27,17 @@ const onSendMessageCallback = async (socket, message) => {
     });
     emitToUser(message.receiver_id, RECEIVE_MESSAGE_EVENT, message);
     emitToUser(currentUserId, RECEIVE_MESSAGE_EVENT, message);
+    const receiverUser = await UserService.find(message.receiver_id);
+    emitToUser(message.receiver_id, USER_MESSAGE_NOTIFICATION_EVENT, { avatar: receiverUser.avatars?.[0]?.value , 
+      msg: `You have received a message from ${receiverUser.first_name} ${receiverUser.last_name}`, title: 'new message' });
+    const notificationModel = new NotificationModel();
+    notificationModel.insert({
+      seen: false,
+      sender_id: currentUserId,
+      receiver_id: message.receiver_id,
+      type: 'new message',
+      content: `You have received a message from ${receiverUser.first_name} ${receiverUser.last_name}`
+    });
     console.log('message sent', message);
   }
   catch (err) {
