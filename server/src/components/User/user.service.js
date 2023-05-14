b74159f66bc4a82f78d6787b3f7b9e149eab6455
Houssam-Eddine Moumanis <houssam.moumanis@gmail.com>
2023-05-14 18:01:56 +0100

    fix - error while not setting currentUserId to find function

--- server/src/components/User/user.service.js
+++ server/src/components/User/user.service.js
@@ -70,13 +70,13 @@ const UserService = {
     ) AS distance
     from users
     where id = $4`;
-    if (userId != currentUserId) {
+    if (currentUserId && userId != currentUserId) {
       let distance = await query(distance_query, [
-        user.location.lat,
-        user.location.lng,
-        user.location.lat,
-        currentUserId
-      ]);
+          user.location.lat,
+          user.location.lng,
+          user.location.lat,
+          currentUserId
+        ]);
       distance = distance.rows[0].distance;
       user.distance = distance;
     }
