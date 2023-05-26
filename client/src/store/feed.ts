4138e0619eb5e4a71518497119a1229c55abff19
Houssam-Eddine Moumanis <houssam.moumanis@gmail.com>
2023-05-26 23:43:17 +0100

    fix - fix no default avatar and user with no avatar like.

--- client/src/store/feed.ts
+++ client/src/store/feed.ts
@@ -13,6 +13,10 @@
 import feedService from '@/services/feedService';
 import userService from '@/services/userService';
 import { defineStore } from 'pinia';
+import { useUserStore } from '@/store/user';
+import { useToast } from 'vue-toastification';
+const toast = useToast();
+
 
 export const useFeedStore = defineStore('feed', {
 	state: () => ({
@@ -35,13 +39,23 @@ export const useFeedStore = defineStore('feed', {
 			this.currentProfile = this.profilesQueue[0];
 		},
 		async likeUser() {
-			await this.showNextProfile();
+			const {currentUser} = useUserStore();
+			if (currentUser.avatars.length < 1) {
+				toast.error('You need to upload at least one photo to like someone');
+				return ;
+			}
 			const { id } = this.currentProfile;
+			await this.showNextProfile();
 			feedService.likeUser(id);
 		},
 		async unLikeUser() {
-			this.showNextProfile();
+			const {currentUser} = useUserStore();
+			if (currentUser.avatars.length < 1) {
+				toast.error('You need to upload at least one photo to dislike someone');
+				return ;
+			}
 			const { id } = this.currentProfile;
+			this.showNextProfile();
 			feedService.unLikeUser(id);
 		},
 		async reportUser(userId) {
